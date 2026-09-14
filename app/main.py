3f9be5324867bb969bb9755d46a91c7d28a66502
mrtvtb <mrtvtb@mail.com>
2026-09-14 21:13:47 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 def copy_file(command: str) -> None:
     if len(command) > 0:
-        command_to_list = command.split(" ")
-        if len(command_to_list) == 3:
-            needed_command = command_to_list[0]
-            file_name = command_to_list[1]
-            new_file_name = command_to_list[2]
+        tokens = command.split(" ")
+        if len(tokens) == 3:
+            needed_command = tokens[0]
+            file_name = tokens[1]
+            new_file_name = tokens[2]
             if needed_command == "cp" and file_name != new_file_name:
                 try:
-                    with open(file_name, "r") as file_in:
-                        with open(new_file_name, "w") as file_out:
-                            data = file_in.read()
-                            file_out.write(data)
+                    with (open(file_name, "r") as file_in,
+                            open(new_file_name, "w") as file_out):
+                        data = file_in.read()
+                        file_out.write(data)
                 except FileNotFoundError:
                     pass
