729b42c66118c6a65691aeb6011c847546432b8b
mrtvtb <mrtvtb@mail.com>
2026-09-14 21:07:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command) > 0:
+        command_to_list = command.split(" ")
+        if len(command_to_list) == 3:
+            needed_command = command_to_list[0]
+            file_name = command_to_list[1]
+            new_file_name = command_to_list[2]
+            if needed_command == "cp" and file_name != new_file_name:
+                try:
+                    with open(file_name, "r") as file_in:
+                        with open(new_file_name, "w") as file_out:
+                            data = file_in.read()
+                            file_out.write(data)
+                except FileNotFoundError:
+                    pass
